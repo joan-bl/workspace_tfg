d4b2f5715bf6017e67b938376b652c34069a27f5
joan-bl <joan.blanch.jimenez@gmail.com>
2025-04-07 18:58:34 +0200

    Update start_project.bat

--- start_project.bat
+++ start_project.bat
@@ -1,6 +1,6 @@
 @echo off
 call C:\Users\joanb\anaconda3\Scripts\activate.bat
 call conda activate TFG
-cd C:\Users\joanb\OneDrive\Escritorio\TFG\workspace
+cd C:\Users\joanb\OneDrive\Escritorio\TFG\Workspace_tfg
 start cmd /k "conda activate TFG && title TFG Project"
 code .
