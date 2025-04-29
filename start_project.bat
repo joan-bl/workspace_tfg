20e0ea161646623303b313c994050f013fead944
joan-bl <joan.blanch.jimenez@gmail.com>
2025-04-29 11:00:02 +0200

    organizacion

--- start_project.bat
+++ start_project.bat
@@ -1,6 +1,6 @@
 @echo off
 call C:\Users\joanb\anaconda3\Scripts\activate.bat
 call conda activate TFG
-cd C:\Users\joanb\OneDrive\Escritorio\TFG\Workspace_tfg
+cd C:\Users\joanb\OneDrive\Escritorio\TFG\Workspace_tfg\histology_bone_analyzer
 start cmd /k "conda activate TFG && title TFG Project"
-code . "breaking_app\cuadrantes-analyzer.py"
+code . "apps\1detection_app\detection_app.py"
